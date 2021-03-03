976dabea5d4e7fb2c0652e560d63e267733e39a5
ryoya-mineshima <76557920+ryoya-mineshima@users.noreply.github.com>
2021-03-03 22:00:33 +0900

    Revert "Basic認証を導入"
    This reverts commit 709bfa469fcb57e29f40b5543947767337c6f2f5.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth     #動作確認中、面倒なので一旦消しておく。下のdef basic_authも消してあるので注意
-
-
-
-
-
-
-
-
-
-
-
-  private
-
-
-
-
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == 'tech' && password == '32838'
-    end
-  end
 end
