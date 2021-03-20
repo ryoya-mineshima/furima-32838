2e675a8a3a25786afbd3830abf4fa2a17eae9144
ryoya-mineshima <76557920+ryoya-mineshima@users.noreply.github.com>
2021-03-20 21:46:52 +0900

    商品情報編集　修正（一部詳細表示含む）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_path, only: [:edit, :update]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -19,22 +21,17 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-    redirect_to root_path if current_user.id != @item.user_id
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path 
     else
       render :edit
     end
-    redirect_to root_path if current_user.id != @item.user_id
   end
 
 
@@ -42,4 +39,12 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
   end
-end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def set_path
+    redirect_to root_path if current_user.id != @item.user_id
+    end
+  end
