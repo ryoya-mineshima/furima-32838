f6c14a727c46cb91c0a210557e24fb38fc6bc88b
ryoya-mineshima <76557920+ryoya-mineshima@users.noreply.github.com>
2021-03-29 15:35:25 +0900

    挙動確認後の修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -4,11 +4,10 @@ class OrderAddress
 
   with_options presence: true do
     validates :post_cord, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
-    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :address
-    validates :phone_number,format: {
-       with: /\A(((0(\d{1}[-(]?\d{4}|\d{2}[-(]?\d{3}|\d{3}[-(]?\d{2}|\d{4}[-(]?\d{1}|[5789]0[-(]?\d{4})[-)]?)|\d{1,4}\-?)\d{4}|0120[-(]?\d{3}[-)]?\d{3})\z/}
+    validates :phone_number,format: {with: /\A[0-9]{11}\z/}
     validates :token
     validates :user_id
     validates :item_id
